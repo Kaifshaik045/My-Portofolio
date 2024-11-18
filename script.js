7b65507c82b893f91fb23f535917e372b45341a4
Kaifshaik045 <kaif48251@gmail.com>
2024-11-18 09:37:13 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Example of dynamically handling any clicks or interactions in the future
+document.querySelector('.view-resume').addEventListener('click', () => {
+    alert("Redirecting to resume...");
+});
